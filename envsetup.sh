1c564f35d46c09013c184e896320082ca18759d0
Chet Kener <Cl3Kener@gmail.com>
2015-08-30 18:39:39 -0400

    Revert "prebuilt chromium:make sure to call the function after setting the environment variable"
    This reverts commit 99d8251e8f95dc6942df52feb77040547d7286db.

--- envsetup.sh
+++ envsetup.sh
@@ -541,8 +541,6 @@ function lunch()
 
     echo
 
-    set_stuff_for_environment
-
     if [[ $USE_PREBUILT_CHROMIUM -eq 1 ]]; then
         chromium_prebuilt
     else
@@ -550,6 +548,7 @@ function lunch()
         export PRODUCT_PREBUILT_WEBVIEWCHROMIUM=""
     fi
 
+    set_stuff_for_environment
     printconfig
 }
 
