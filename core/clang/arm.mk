9456327785ffb85c80f65bf8d2e0c9f57634357b
tetsuo55 <tetsugit@gmail.com>
2016-03-15 08:27:33 +0100

    Revert "DragonTC and Polly support Test 8"
    Requires too many changes to libcxx and bionic
    
    This reverts commit 754d7064333ae615e6d10ded27dbe40291682a70.

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -1,27 +1,16 @@
 # Clang flags for arm arch, target or host.
 
-CLANG_CONFIG_arm_EXTRA_ASFLAGS :=
+CLANG_CONFIG_arm_EXTRA_ASFLAGS := \
+  -no-integrated-as
 
-CLANG_CONFIG_arm_EXTRA_CFLAGS :=
+CLANG_CONFIG_arm_EXTRA_CFLAGS := \
+  -no-integrated-as
 
-ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
-  # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
-  # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
-  ifneq ($(TARGET_DRAGONTC_VERSION),)
-    CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait2 -mfpu=neon-vfpv4
-  else
-    CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait
-  endif
-endif
+CLANG_CONFIG_arm_EXTRA_CPPFLAGS := \
+  -no-integrated-as
 
-ifeq ($(HOST_OS),darwin)
-  # Darwin is really bad at dealing with idiv/sdiv. Don't use krait on Darwin.
-  CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=cortex-a9
-endif
-
-CLANG_CONFIG_arm_EXTRA_CPPFLAGS :=
-
-CLANG_CONFIG_arm_EXTRA_LDFLAGS :=
+CLANG_CONFIG_arm_EXTRA_LDFLAGS := \
+  -no-integrated-as
 
 # Include common unknown flags
 CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
@@ -30,19 +19,14 @@ CLANG_CONFIG_arm_UNKNOWN_CFLAGS := \
   -fgcse-after-reload \
   -frerun-cse-after-loop \
   -frename-registers \
-  -fno-align-jumps \
   -fno-builtin-sin \
-  -fno-caller-saves \
-  -fno-early-inlining \
-  -fno-move-loop-invariants \
-  -fno-partial-inlining \
   -fno-strict-volatile-bitfields \
-  -fno-tree-copy-prop \
-  -fno-tree-loop-optimize \
+  -fno-align-jumps \
   -Wa,--noexecstack
 
 define subst-clang-incompatible-arm-flags
   $(subst -march=armv5te,-march=armv5t,\
   $(subst -march=armv5e,-march=armv5,\
-  $(1)))
+  $(subst -mcpu=cortex-a15,-march=armv7-a,\
+  $(1))))
 endef
